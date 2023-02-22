0d7e23e98212b1e179d4c6485462dfbfe751ce96
snail-001 <52124912+snail-001@users.noreply.github.com>
2023-02-22 15:55:45 -0800

    started revision
    revising my bounty generation, to enum, and will be working on public class that holds it all and a 2d array that compares elements that make it up to determine the bounty risk

--- Project Oligarch/Assets/cameron scripts/generateBounties.cs
+++ Project Oligarch/Assets/cameron scripts/generateBounties.cs
@@ -7,82 +7,69 @@ using System;
 
 public class generateBounties : MonoBehaviour
 {
-    //
-    // this is a little spagetti but i couldnt get them to nest, however i did in a round about way get them to randomize,
-    // now i just need to bind these to an object and see if they work....maybe? 
-    //
-    //?DONE---------
-    //! Which Planet they venture to.
-    //! What type of Bounty it is.
-    //! The risk of the bounty, dictating time limit and enemy difficulty.
-    //!    ---time
-    //!    ---enemy difficulty
-    //! The payout at the end of the Bounty.
-    //! ----- If there are sub-objectives that increase the end payout 
-
-    public string[] planets = {"earth" , "mars" , "venus" , "jupiter"};
-    
-    public string[] bountyType = { "kill", "capture", "rescue" };
-
-    public int[] bountyRisk = { 10, 20, 30, 50 };
-
-    public string[] time = { "1min", "2min", "3min", "5min" };
-
-    public string[] enemyDif = { "easy", "medium", "hard", "impossible" };
-
-    public int[] payout = { 100, 200, 300, 500 };
-
-    public string[] subObjectives = { " fastest time", "dont get seen", "dont get hit", "find all loot" };
-
-    System.Random rnd = new System.Random();
-
-        
-    public void Awake()
+    public enum planets
     {
         
-        int planetLength = planets.Length;
-        int bountTypeLength = bountyType.Length;
-        int bountRiskLength = bountyRisk.Length;
-        int timeLenght = time.Length;
-        int enDifLength = enemyDif.Length;
-        int payLength = payout.Length;
-        int subObjLength = subObjectives.Length;
+        earth,
+        mars,
+        venus,
+        jupiter
+    }
+    public enum bountyType
+    {
 
-        //! this grabs random array number (following max number of list elements) and then prints the number
-       
-        int rndPlanet = rnd.Next( 0, planetLength );
-        int rndBType = rnd.Next( 0, bountTypeLength );
-        int rndBRisk= rnd.Next( 0, bountRiskLength );
-        int rndTime= rnd.Next( 0, timeLenght );
-        int rndEDif= rnd.Next( 0, enDifLength );
-        int rndPay= rnd.Next( 0, payLength );
-        int rndSubObj= rnd.Next( 0, subObjLength );
+        Kill,
+        Capture,
+        Rescue
+    }
+    //public enum payout
+    //{
+    //    100,
+    //    200,
+    //    300,
+    //    400
+    //}
 
-        //! grabs the new number and throws it into the list 
-        Debug.Log( "planet: " + planets[rndPlanet] );
-        Debug.Log( "bounty type: " + bountyType[rndBType] );
-        Debug.Log( "bounty risk: " + bountyRisk[rndBRisk] );
-        Debug.Log( "time: " + time[rndTime] );
-        Debug.Log( "difficulty: " + enemyDif[rndEDif] );
-        Debug.Log( "payout: " + payout[rndPay] );
-        Debug.Log( "sub objectives: " + subObjectives[rndSubObj] );
 
-        Debug.Log( "-----------------------------" );
 
-        
-        //! prints the whole string finally
-        foreach (var name in bountyRisk)
-        {
-            Debug.Log("bounty risk = " +  name );
-        }
-        
+
+    public void Awake()
+    {
+
+        Debug.Log((planets)1);
+               
+        //public string[] planets = {"earth" , "mars" , "venus" , "jupiter"};
+
+        //public string[] bountyType = { "kill", "capture", "rescue" };
+
+        //public int[] bountyRisk = { 10, 20, 30, 50 };
+
+        //public string[] time = { "1min", "2min", "3min", "5min" };
+
+        //public string[] enemyDif = { "easy", "medium", "hard", "impossible" };
+
+        //public int[] payout = { 100, 200, 300, 500 };
+
+        //public string[] subObjectives = { " fastest time", "dont get seen", "dont get hit", "find all loot" };
+
     }
 
+    
 
-    //!ideas: 
-    //?a list for all the bounties
-    //?a list of each bounty member with its qualites in it that are public variables
-    //? how to make a list a child of another list but have them remain editable?
-    //TODO ----way to make the debug work:
-   
+
 }
+
+//
+//wyatt says:
+//use enums for all the string parts
+//use "get" and "switch" functions to fill said enums
+//everything should be in a bounty class that is static, and can be called by other things
+//
+//the time it takes in the 2d array the lower value would be an arbitrary difficulty and then multiply around to find the formor and latter value
+//  to then build the difficulty from the enemies and such
+
+
+
+
+
+
