7d11736305050d034aabd8f1d3636c271552be63
snail-001 <52124912+snail-001@users.noreply.github.com>
2023-03-08 17:36:27 -0800

    finished the 2d array and other fix
    finished the 2d array and the maths for pulling only some of the options based on the bounty risk.
    then i spoke with the designers for what bounty is random and what is scaling, then added that in.

--- Project Oligarch/Assets/cameron scripts/generateBounties.cs
+++ Project Oligarch/Assets/cameron scripts/generateBounties.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-
+using JetBrains.Annotations;
 
 
 public class generateBounties : MonoBehaviour
 {
+    //planets
     public enum planets
     {
         Earth,
         Mars,
         Venus,
         Jupiter
+       
     };
+    //creating the drop down list
+    public planets Planets;
+
     public enum bountyType
     {
 
@@ -21,21 +26,31 @@ public class generateBounties : MonoBehaviour
         Capture,
         Rescue
     };
-  
+    //creating the drop down list
+    public bountyType types;
+
     public enum basePayout
     {
-        money1,
-        money2,
-         money3,
-         money4
+        OneHundred =   100,
+        TwoHundred =   200,
+        ThreeHundred = 300,
+        FourHundred =  400
     };
+    //creating the drop down list
+    public basePayout payoutBase;
+
     public enum bountyRisk
     {
-        low,
-        medium,
-        extreme
+        Low,
+        Medium,
+        Extreme
     }
-    
+    //creating the drop down list
+    public bountyRisk risks;
+    //---------depreceated but might be useful idk-----bountyRisk LRisk = bountyRisk.Low;    <---this functions and is close to what i want
+  
+
+
     //time
     public enum timeLimit
     {
@@ -43,123 +58,108 @@ public class generateBounties : MonoBehaviour
         Average,
         Exceptional
     }
-   
+    //creating the drop down list
+    public timeLimit time;
+
+
+
     //Enemydifficulty 
     public enum enemyDifficulty
     {
-        stage1,
-        stage2,
-        stage3,
-        stage4,
-        stage5
+        Stage1,
+        Stage2,
+        Stage3,
+        Stage4,
+        Stage5
     }
+    //creating the drop down list
+    public enemyDifficulty EnemyDifficulty;
+
+    //turning the base payouts into a method that can be multiplied later
+    int scalePayout100 = (int)basePayout.OneHundred;
+    int scalePayout200 = (int)basePayout.TwoHundred;
+    int scalePayout300 = (int)basePayout.ThreeHundred;
+    int scalePayout400 = (int)basePayout.FourHundred;
+
+
+    //the line that creates the item used to randomize things later
+    System.Random rnd = new System.Random();
+    //the assignment of a new name to use for calling the random version of these enums
+    private planets rndPlanets;
+    private bountyType rndBounty;
     
-    public float planning()
+
+    public void Start()//for testing if it works // no core mechanics allowe here.
     {
-         string[,] risk = new string[2, 5];
+        
+        //does the math for the scaling of the payout for the "exceptional" time limit
+        int exceptionalMath = (int)(scalePayout100 * 1.5f);
+        Debug.Log("100*1.5 = " + exceptionalMath);
+
+        //does the math for the average payout of the "average" time limit
+        int averageMath = (int)(scalePayout100 * 1f);
+        Debug.Log("100 * 1 = " + averageMath);
+
+        //does the math for the "poor" scaling payout time limit
+        int poorMath = (int)(scalePayout100 * .5f);
+        Debug.Log("100 * .5 = " + poorMath);
+
+        //the array that holds the time limits info in column 0, and the enemy difficulty stage in column 1
+        string[,] risk = new string[2, 5];
 
         risk[0, 0] = "1 min";
         risk[0, 1] = "2 min";
         risk[0, 2] = "3 min";
         risk[0, 3] = "4 min";
         risk[0, 4] = "5 min";
-      
-    //--------------
-    
+
+        //--------------
+
         risk[1, 0] = "stage1";
         risk[1, 1] = "stage2";
-        risk[1, 2] = "stage3";   
+        risk[1, 2] = "stage3";
         risk[1, 3] = "stage4";
         risk[1, 4] = "stage5";
 
-        if (bountyRisk == bountyRisk.low)
-        {
-            Debug.Log("the time limit is " + risk[0,0] + "enemy difficulty is " + risk[1,0]);
-        }
-    }
-
-
-        public void Awake()//for testing if it works // no core mechanics allowe here.
-    {
+        //makes random selections for the enums------------------------------
+        rndPlanets = (planets)Random.Range(0, 4);
+        rndBounty = (bountyType)Random.Range(0, 3);
+    
+     //testing the selection of the enums from the drop downs
+        Debug.Log("the chosen bounty type is " + types);
+        Debug.Log("the chosen base pay is " + payoutBase);
         
-            
-        //Debug.Log("planet type is " + (planets)Random.Range(0, 4));
-        //Debug.Log("bounty type is " + (bountyType)Random.Range(0, 3));       
-        //Debug.Log("base payout is " + (basePayout)Random.Range(0, 4));
-       
 
+
+       
+        //the function that checks what risk is selected and outputs a set of time limits and an enemy stage difficulty
+        if (risks == bountyRisk.Low)
+        {
+
+            Debug.Log("the time limit is " + risk[0, 0] + "  The enemy difficulty is " + risk[1, 0]);
+
+        }
+        else if (risks == bountyRisk.Medium)
+        {
+            Debug.Log("the time limit is " + risk[0, 3] + "  The enemy difficulty is " + risk[1, 3]);
+        }
+        else if (risks == bountyRisk.Extreme)
+        {
+            Debug.Log("the time limit is " + risk[0, 4] + "  The enemy difficulty is " + risk[1, 4]);
+        }
+        else
+        {
+            Debug.Log("not a set difficulty, check code, broken");
+        }
+
+        //spacing for visuals 
         Debug.Log("==========================");
 
+
+        //random elements test
+        Debug.Log("random planets " + rndPlanets);
+
+        Debug.Log("random bounty is " + rndBounty);
        
-       // Debug.Log("enemey difficulty " + risk[1,3] + " and time limit " + risk[0,3]); //!was about to test whether this debug works to print the whole list as a debug, to check if my array makes sense
     }
-}
-        
-       // bountyRisk risk = bountyRisk.low; //for testing if the swapping works
-        //
-        //if (risk == bountyRisk.low)
-        //{
-            //        timeLimit.Exceptional <= 4f; ----wrote this before the time enum
-        //    Debug.Log(timeLimit.Exceptional + " 4min cutoff");
-         //   Debug.Log("--------");//spacing
-          //  Debug.Log(enemyDifficulty.stage1);
-        //}
-        //else
-        //{
-        //    Debug.Log("fail");
-       // }
-        
-
-        //public int[] payout = { 100, 200, 300, 500 }; ---multiplied by how fast complete,
-        //tiers
-        //extreme risk
-        //stronger enemy tier
-        //except..1-4min
-        //avg...4-8min
-        //poor...8min+
-        //medium risk
-        //except..1-5min
-        //avg...5-10min
-        //poor...10min+
-        //low risk
-        ////except..1-6min
-        //avg...6-12min
-        //poor...12min+
-
-        //        //payout is calculated when the boss is killed (the timer stops then) payout = amount of coins on the reward multiplied by the tiers
-        //exceptional = 1.5x
-        //average = 1x
-        //poor = .2x
-
-
-        //public string[] subObjectives = { " fastest time", "dont get seen", "dont get hit", "find all loot" };
-
-        //optional
-        //kill 100 enemies
-        //random item rewards
-        //dont get hit
-        // 50% more rewards
-        //dont open shop
-        //2x rewards and random chest
-   // }
-
-    
-
-
-//}
-
-//
-//wyatt says:
-//use enums for all the string parts
-//use "get" and "switch" functions to fill said enums
-//everything should be in a bounty class that is static, and can be called by other things
-//
-//the time it takes in the 2d array the lower value would be an arbitrary difficulty and then multiply around to find the formor and latter
-//value
-//  to then build the difficulty from the enemies and such
-
-
-
-
-
-
+}     
