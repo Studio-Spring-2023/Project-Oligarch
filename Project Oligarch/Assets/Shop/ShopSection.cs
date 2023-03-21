0e04f39da01faf5728bb7c609bb3c3b330d791b4
florenzo568 <florenzo568@gmail.com>
2023-03-21 15:30:49 -0700

    added sin wave hover to items

--- Project Oligarch/Assets/Shop/ShopSection.cs
+++ Project Oligarch/Assets/Shop/ShopSection.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class ShopSection : MonoBehaviour
 {
     public ShopItem CurrItem;
+    public Transform HoverPoint;
+    private Vector3 startPoint;
+    public float HoverHeight;
+    public float HoverSpeed;
     private float Price;
+    [SerializeField] private GameObject Item;
     void Start()
     {
         Price = CurrItem.price;
+        Item = Instantiate(CurrItem.DisplayPrefab, HoverPoint.position, Quaternion.identity);
+        startPoint = HoverPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HoverItem();
+    }
+
+    private void HoverItem()
+    {
+        float sinWave = Mathf.Sin(Time.time * HoverSpeed) * HoverHeight;
+        Item.transform.position =  startPoint + transform.up * Mathf.Sin(Time.time * HoverSpeed) * HoverHeight;
     }
 }
