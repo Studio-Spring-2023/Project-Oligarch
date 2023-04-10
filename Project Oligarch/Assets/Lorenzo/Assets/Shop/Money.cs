eb0a0ed6e56b8d6271309d1732839b579dda5db0
florenzo568 <florenzo568@gmail.com>
2023-04-10 15:36:57 -0700

    added models into unity + Fixed money display

--- Project Oligarch/Assets/Lorenzo/Assets/Shop/Money.cs
+++ Project Oligarch/Assets/Lorenzo/Assets/Shop/Money.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Money : MonoBehaviour
 {
     public int Credits;
+    [SerializeField] TextMeshProUGUI Text;
+
+    void Update()
+    {
+        Text.text = Credits.ToString();
+    }
 
 
     // Update is called once per frame
