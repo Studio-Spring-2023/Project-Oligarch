9e4dc9dcd516afa105d399a877b170dc574b7033
Heather Neumann <DarkKittrena@gmail.com>
2023-04-13 12:50:50 -0700

    comments added

--- Project Oligarch/Assets/Scripts/Heather_Scripts/Editor/FoVEditor.cs
+++ Project Oligarch/Assets/Scripts/Heather_Scripts/Editor/FoVEditor.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(FieldofView))]
+[CustomEditor ( typeof ( FieldofView ) )]
 public class FoVEditor : Editor
 {
     private void OnSceneGUI ( )
     {
-        FieldofView FoV = (FieldofView)target;
-
+        FieldofView FoV = ( FieldofView ) target;
         
+        //This is the handles for the (not visible) circle field of view
         Handles.color = Color.white;
-        Handles.DrawWireArc (FoV.transform.position , Vector3.up , Vector3.forward , 0 ,FoV.viewRadius );
+        Handles.DrawWireArc ( FoV.transform.position , Vector3.up , Vector3.forward , 0 , FoV.viewRadius );
 
-        Vector3 viewAngleA =FoV.DirFromAngle ( -FoV.viewAngle / 2 , false );
-        Vector3 viewAngleB =FoV.DirFromAngle (FoV.viewAngle / 2 , false );
-        Handles.DrawLine (FoV.transform.position ,FoV.transform.position + viewAngleA *FoV.viewRadius );
-        Handles.DrawLine (FoV.transform.position ,FoV.transform.position + viewAngleB *FoV.viewRadius );
+        //This is the handles and math for the frontal cone field of view that is used for the missiles
+        Vector3 viewAngleA = FoV.DirFromAngle ( -FoV.viewAngle / 2 , false );
+        Vector3 viewAngleB = FoV.DirFromAngle ( FoV.viewAngle / 2 , false );
+        Handles.DrawLine ( FoV.transform.position , FoV.transform.position + viewAngleA * FoV.viewRadius );
+        Handles.DrawLine ( FoV.transform.position , FoV.transform.position + viewAngleB * FoV.viewRadius );
     }
 
 }
--- Project Oligarch/Assets/Scripts/Heather_Scripts/FieldofView.cs
+++ Project Oligarch/Assets/Scripts/Heather_Scripts/FieldofView.cs
@@ -5,31 +5,35 @@ using UnityEngine;
 
 public class FieldofView : MonoBehaviour
 {
-    [Range(0,5)]
+    //These are for controlling the field of view
+    [Range ( 0 , 5 )]
     public float viewRadius;
-    [Range(0,100)]
+    [Range ( 0 , 100 )]
     public float viewAngle;
 
+    //These are refrences for the missile
     public LayerMask Enemy;
     public GameObject HomingMissile;
     public HomingMissile missile;
-
 
     private void Update ( )
     {
         MissileTargeting ( );
     }
 
-    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal )
+    public Vector3 DirFromAngle ( float angleInDegrees , bool angleIsGlobal )
     {
+        //This makes it so the field of view can turn with the player
         if ( !angleIsGlobal )
         {
             angleInDegrees += transform.eulerAngles.y;
         }
 
-        return new Vector3 ( Mathf.Sin ( angleInDegrees * Mathf.Deg2Rad ) ,0 , Mathf.Cos ( angleInDegrees * Mathf.Deg2Rad ) );
+        //This is the math for the field of view
+        return new Vector3 ( Mathf.Sin ( angleInDegrees * Mathf.Deg2Rad ) , 0 , Mathf.Cos ( angleInDegrees * Mathf.Deg2Rad ) );
     }
 
+    //This is the math for the missile to use the field of view to aquire a target after spawning
     public void MissileTargeting ( )
     {
         Collider [ ] targetsInViewRadius = Physics.OverlapSphere ( transform.position , viewRadius , Enemy );
@@ -37,7 +41,7 @@ public class FieldofView : MonoBehaviour
         for ( int i = 0 ; i < targetsInViewRadius.Length ; i++ )
         {
             Transform target = targetsInViewRadius [ i ].transform;
-            Vector3 dirToTarget=(target.position - transform.position ).normalized;
+            Vector3 dirToTarget = ( target.position - transform.position ).normalized;
 
             if ( Vector3.Angle ( transform.forward , dirToTarget ) < viewAngle / 2 )
             {
