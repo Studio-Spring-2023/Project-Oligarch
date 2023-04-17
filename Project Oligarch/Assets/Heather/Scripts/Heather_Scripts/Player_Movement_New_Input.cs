3f4a7cae5e82e007cebea37b630deb2d6fc289b5
florenzo568 <florenzo568@gmail.com>
2023-04-17 14:37:31 -0700

    WIP heather merge

--- Project Oligarch/Assets/Heather/Scripts/Heather_Scripts/Player_Movement_New_Input.cs
+++ Project Oligarch/Assets/Heather/Scripts/Heather_Scripts/Player_Movement_New_Input.cs
@@ -3,58 +3,58 @@ using static Models;
 
 public class Player_Movement_New_Input : MonoBehaviour
 {
-    PlayerInput playerInput;
-    CharacterController playerControl;
-    public Cam_Control camControl;
-    public Transform camTarget;
-    public PlayerControl settings;
+    //PlayerInput playerInput;
+    //CharacterController playerControl;
+    //public Cam_Control camControl;
+    //public Transform camTarget;
+    //public PlayerControl settings;
 
-    [HideInInspector]
-    public Vector2 input_Move;
-    [HideInInspector]
-    public Vector2 input_Look;
+    //[HideInInspector]
+    //public Vector2 input_Move;
+    //[HideInInspector]
+    //public Vector2 input_Look;
 
-    Vector3 playerWalk;
+    //Vector3 playerWalk;
 
-    public bool isLookTarget;
+    //public bool isLookTarget;
 
-    private void Awake ( )
-    {
-        playerInput = new PlayerInput ( );
-        playerControl = GetComponent<CharacterController> ( );
+    //private void Awake ( )
+    //{
+    //    playerInput = new PlayerInput ( );
+    //    playerControl = GetComponent<CharacterController> ( );
 
-        playerInput.Movement.Move.performed += x => input_Move = x.ReadValue<Vector2> ( );
-        playerInput.Movement.View.performed += x => input_Look = x.ReadValue<Vector2> ( );
-        playerInput.Actions.Jump.performed += x => Jump ( );
+    //    playerInput.Movement.Move.performed += x => input_Move = x.ReadValue<Vector2> ( );
+    //    playerInput.Movement.View.performed += x => input_Look = x.ReadValue<Vector2> ( );
+    //    playerInput.Actions.Jump.performed += x => Jump ( );
 
-        playerInput.Enable ( );
-    }
+    //    playerInput.Enable ( );
+    //}
 
-    private void Update ( )
-    {
-        Move ( );
-    }
+    //private void Update ( )
+    //{
+    //    Move ( );
+    //}
 
-    private void Move ( )
-    {
-        playerWalk = camControl.transform.forward * ( settings.forwardSpeed * input_Move.y ) * Time.deltaTime;
-        playerWalk += camControl.transform.right * ( settings.forwardSpeed * input_Move.x ) * Time.deltaTime;
+    //private void Move ( )
+    //{
+    //    playerWalk = camControl.transform.forward * ( settings.forwardSpeed * input_Move.y ) * Time.deltaTime;
+    //    playerWalk += camControl.transform.right * ( settings.forwardSpeed * input_Move.x ) * Time.deltaTime;
 
-        if ( !isLookTarget )
-        {
-            var originalRotate=transform.rotation;
-            transform.LookAt ( playerWalk + transform.position , Vector3.up );
-            var newRotate=transform.rotation;
+    //    if ( !isLookTarget )
+    //    {
+    //        var originalRotate=transform.rotation;
+    //        transform.LookAt ( playerWalk + transform.position , Vector3.up );
+    //        var newRotate=transform.rotation;
 
-            transform.rotation = Quaternion.Lerp ( originalRotate , newRotate , settings.playerSmooth );
-        }
+    //        transform.rotation = Quaternion.Lerp ( originalRotate , newRotate , settings.playerSmooth );
+    //    }
 
-        playerControl.Move ( playerWalk );
-    }
+    //    playerControl.Move ( playerWalk );
+    //}
 
-    private void Jump ( )
-    {
-        Debug.Log ( "I'm Jumping" );
-    }
+    //private void Jump ( )
+    //{
+    //    Debug.Log ( "I'm Jumping" );
+    //}
 
 }
