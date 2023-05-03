0125c3ad2e1a1eff7ef1ec057b4d265be01665d9
Gkkillzone <97559223+Gkkillzone@users.noreply.github.com>
2023-05-02 22:05:07 -0700

    zach scene changes

---
+++ Project Oligarch/Assets/Scripts/Managers/CutsceneManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneManager : MonoBehaviour
+{
+
+    [SerializeField]
+    List<GameObject> enableOnEnd = new List<GameObject>();
+    [SerializeField]
+    List<GameObject> disableOnEnd = new List<GameObject>();
+    [SerializeField]
+    float time;
+    void Start()
+    {
+        foreach (GameObject obj in enableOnEnd)
+        {
+            obj.SetActive(false);
+        }
+        StartCoroutine(cutsceneTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator cutsceneTimer()
+    {
+        yield return new WaitForSeconds(time);
+        foreach(GameObject obj in enableOnEnd)
+        {
+            obj.SetActive(true);
+        }
+
+        foreach (GameObject obj in disableOnEnd)
+        {
+            obj.SetActive(false);
+        }
+
+    }
+}
