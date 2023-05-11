75e56ba5cca89d646172c25470e4decbf01decb1
Zach Thanstrom <97559223+Gkkillzone@users.noreply.github.com>
2023-05-10 20:10:45 -0700

    Explosive barrel code/prefab implementation

---
+++ Project Oligarch/Assets/Scripts/Mobs/ExplosiveBarrel.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveBarrel : MonoBehaviour
+{
+    
+    public float neededSpeed;
+    public GameObject explosion;
+    public float force, radius;
+    public float lingerTime = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Velocity:" + this.gameObject.GetComponent<Rigidbody>().velocity.magnitude);
+    }
+
+    void OnTriggerEnter()
+    {
+        if (this.gameObject.GetComponent<Rigidbody>().velocity.magnitude > neededSpeed)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        GameObject _exp = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(_exp, lingerTime);
+        knockBack();
+        Destroy(gameObject, .2f);
+    }
+
+    void knockBack()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearby in colliders)
+        {
+            Rigidbody rigg = nearby.GetComponent<Rigidbody>();
+            if(rigg != null)
+            {
+                rigg.AddExplosionForce(force, transform.position, radius);
+            }
+
+        }
+    }
+}
