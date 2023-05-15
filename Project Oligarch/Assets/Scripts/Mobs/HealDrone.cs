6acbabbb80d14734606e08fc4f6cb3a747b11719
Zach Thanstrom <97559223+Gkkillzone@users.noreply.github.com>
2023-05-15 15:47:08 -0700

    Heal drone death functionality added

--- Project Oligarch/Assets/Scripts/Mobs/HealDrone.cs
+++ Project Oligarch/Assets/Scripts/Mobs/HealDrone.cs
@@ -5,12 +5,18 @@ using UnityEngine.AI;
 
 public class HealDrone : MonoBehaviour
 {
+    public GameObject[] Allies;
+    public GameObject NearestAlly;
+    float distance;
+    float nearestDistance;
+
     public Transform target;
     NavMeshAgent nav;
     // Start is called before the first frame update
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        Allies = GameObject.FindGameObjectsWithTag("enemies");
     }
 
     // Update is called once per frame
